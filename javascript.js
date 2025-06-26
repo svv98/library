d4ac3c846379902be892a556df5c65717eac4759
svv98 <76674639+svv98@users.noreply.github.com>
2025-06-25 22:36:09 -0700

    Add Book modal added Info Book and Delete Book modals started

--- javascript.js
+++ javascript.js
@@ -14,7 +14,30 @@
 
 // addToLibrary()
 
-const infoModal = document.querySelector(".bookInfo");
+const addModal = document.querySelector("#addBook");
+const addShow = document.querySelector(".addButton");
+const addClose = document.querySelector(".addClose");
+addShow.addEventListener("click", () => {
+  addModal.showModal();
+});
+addClose.addEventListener("click", () => {
+  addModal.close();
+});
+
+
+
+const deleteModal = document.querySelector("#deleteBook");
+const deleteShow = document.querySelector(".deleteButton");
+const deleteClose = document.querySelector(".deleteClose");
+deleteShow.addEventListener("click", () => {
+  deleteModal.showModal();
+});
+deleteClose.addEventListener("click", () => {
+  deleteModal.close();
+});
+
+
+const infoModal = document.querySelector("#bookInfo");
 const infoShow = document.querySelector(".book");
 const infoClose = document.querySelector(".infoClose");
 infoShow.addEventListener("click", () => {
@@ -22,14 +45,4 @@ infoShow.addEventListener("click", () => {
 });
 infoClose.addEventListener("click", () => {
   infoModal.close();
-});
-
-const addModal = document.querySelector(".addBook");
-const addShow = document.querySelector(".add");
-const addClose = document.querySelector(".addClose");
-addShow.addEventListener("click", () => {
-  addModal.showModal();
-});
-addClose.addEventListener("click", () => {
-  addModal.close();
 });
